e9d5e3915887e65aefab5351cc9a28627f987763
Aditya Goyal <145698186+adityagoyal597@users.noreply.github.com>
2026-05-27 22:35:52 +0530

    Add: baseball-game - submission-18

---
+++ Data Structures & Algorithms/baseball-game/submission-18.py
@@ -0,0 +1,21 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        record=[]
+        for i,operation in enumerate(operations):
+            if operation == '+':
+                record.append(record[-1]+record[-2])
+            elif operation == 'C':
+                record.pop()
+            elif operation == 'D':
+                record.append(2*record[-1])
+            else: # Number
+                record.append(int(operation))
+        
+        return sum(record)
+
+        # sum=0
+        # for rec in record:
+        #     sum+=rec
+
+        # return sum
+        
