85027c045abec1eff4f72557c4936d1551a929dd
Aditya Goyal <145698186+adityagoyal597@users.noreply.github.com>
2026-06-05 23:16:19 +0530

    Add: path-sum - submission-8

---
+++ Data Structures & Algorithms/path-sum/submission-8.py
@@ -0,0 +1,19 @@
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        path=[]
+        def Path(root,path):
+            if not root:
+                return False
+            path.append(root.val)
+            if not root.left and not root.right:
+                res = (targetSum==sum(path))
+                if not res: 
+                    path.pop()
+                return res
+            if Path(root.left,path):
+                return True
+            if Path(root.right,path):
+                return True
+            path.pop()
+            return False
+        return Path(root,path)
