5cd52daed43ce5bd9a5873c7cfaff91f69771394
Aditya Goyal <145698186+adityagoyal597@users.noreply.github.com>
2026-06-20 23:26:15 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n<=0:
+            return n
+        
+        dp=[1,1]
+        i=2
+    
+
+        while i<=n:
+            temp=dp[0]
+            dp[0]=dp[0]+dp[1]
+            dp[1]=temp
+            i+=1
+        return dp[0]
+
+        
