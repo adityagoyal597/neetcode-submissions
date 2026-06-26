84a0c670e76d5c580a061d4eb758d443fc18fe39
Aditya Goyal <145698186+adityagoyal597@users.noreply.github.com>
2026-06-26 12:38:51 +0530

    Add: is-palindrome - submission-13

---
+++ Data Structures & Algorithms/is-palindrome/submission-13.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        L,R=0,len(s)-1
+
+        while L<R:
+            #if not (ord('A')<=ord(s[L])<=ord('Z') or ord('a')<=ord(s[L])<=ord('z') or ord('0')<=ord(s[L])<=ord('9')):
+            while L<R and not s[L].isalnum():
+                # using while instead of if as to skip all the contiguous non alphanumeric character rather than just one 
+                L+=1
+            #if not (ord('A')<=ord(s[R])<=ord('Z') or ord('a')<=ord(s[R])<=ord('z') or ord('0')<=ord(s[R])<=ord('9')):
+            while L<R and not s[R].isalnum():    
+                # using while instead of if as to skip all the contiguous non alphanumeric character rather than just one
+                R-=1
+            if s[L].lower()!=s[R].lower():
+                return False
+            # else s[L].lower()==s[R].lower():
+            L+=1
+            R-=1
+        
+        return True
