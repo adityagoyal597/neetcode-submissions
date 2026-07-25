0353cfa73185c68549e704349f1e7188928ff606
Aditya Goyal <145698186+adityagoyal597@users.noreply.github.com>
2026-07-25 13:12:02 +0530

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.py
@@ -0,0 +1,41 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result=[]
+
+        nums.sort()
+
+        for i,firstNumber in enumerate(nums):
+
+            # as the array is sorted if firstNumber becomes positive , all the numbers in the sorted array sfter it will also be positve 
+            # triplet of positive numbers can't be 0
+            
+            if firstNumber>0:
+                break
+            
+            # to skip duplicate triplet answers for duplicate firstNumber
+
+            if firstNumber==nums[i-1] and i>0:
+                continue
+
+            L,R=i+1,len(nums)-1
+
+            while L<R:
+                threeSum=firstNumber+nums[L]+nums[R]
+
+                if threeSum>0:
+                    R-=1
+                elif threeSum<0:
+                    L+=1
+                else:
+                    # threeSum==0
+                    result.append([firstNumber,nums[L],nums[R]])
+
+                    # searching for another pair
+                    L+=1
+                    R-=1
+
+                    while nums[L]==nums[L-1] and L<R:
+                        L+=1
+        
+        return result
+
