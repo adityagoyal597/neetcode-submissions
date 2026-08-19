1d72500831dbe9be76daa85c26a235d81182522f
Aditya Goyal <145698186+adityagoyal597@users.noreply.github.com>
2026-08-19 17:56:54 +0530

    Add: coin-change-ii - submission-2

---
+++ Data Structures & Algorithms/coin-change-ii/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        cache={}
+
+        def dfs(i,amount):
+            if amount==0:
+                return 1
+            
+            if i ==len(coins) or amount<0:
+                return 0
+            
+            if (i,amount) in cache:
+                return cache[(i,amount)]
+            
+            # include curent coin as many times as you want
+            include=dfs(i,amount-coins[i])
+            
+            # skip current coin
+            skip=dfs(i+1,amount)
+
+            cache[(i,amount)]=include+skip
+
+            return cache[(i,amount)]
+        
+        return dfs(0,amount)
