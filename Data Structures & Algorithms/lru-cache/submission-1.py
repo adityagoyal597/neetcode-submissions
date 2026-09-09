a61608b9c3315f35b90ccc9481520b55ba721e58
Aditya Goyal <145698186+adityagoyal597@users.noreply.github.com>
2026-09-09 16:15:44 +0530

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.py
@@ -0,0 +1,57 @@
+class Node:
+
+    def __init__(self,key,val):
+        self.key=key
+        self.val=val
+        self.prev=None
+        self.next=None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+
+        self.capacity=capacity
+        self.cache={}
+        self.left=Node(0,0)
+        self.right=Node(0,0)
+        self.left.next=self.right
+        self.right.prev=self.left
+    
+    def removeNode(self,node):
+        prev=node.prev
+        next=node.next
+        prev.next=next
+        next.prev=prev
+    
+    # inserting before the Right node , making it the MRU
+    def insertNode(self,node):
+        prev=self.right.prev
+        next=self.right
+
+        prev.next=node
+        next.prev=node
+
+        node.next=next
+        node.prev=prev
+        
+
+    def get(self, key: int) -> int:
+
+        if key in self.cache:
+            self.removeNode(self.cache[key])
+            self.insertNode(self.cache[key])
+            return self.cache[key].val
+        return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+
+        if key in self.cache:
+            self.removeNode(self.cache[key])
+        self.cache[key]=Node(key,value)
+        self.insertNode(self.cache[key])
+        
+        if len(self.cache)>self.capacity:
+            lru=self.left.next
+            self.removeNode(lru)
+            del self.cache[lru.key]
